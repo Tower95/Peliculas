6089d2aafec6a06bc0c51e71ddfbdb80958e4f8d
Ismael Torres <torrestmi3@gmail.com>
2024-07-22 14:47:12 -0700

    first commit

---
+++ src/dominio/Movi.java
@@ -0,0 +1,42 @@
+package dominio;
+
+import java.util.Objects;
+
+public class Movi {
+  private String name;
+
+  public Movi(){}
+
+  public Movi(String name){
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Movi movi = (Movi) o;
+    return Objects.equals(name, movi.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(name);
+  }
+
+  @Override
+  public String toString() {
+    return "Movi{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+}
